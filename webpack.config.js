f902d99c5ee83449c343236408bf0a4c05f2d4e2
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2022-08-25 16:51:09 -0700

    add license text

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@
 import webpack from 'webpack'
 // import MiniCssExtractPlugin from "mini-css-extract-plugin"
 
+import fs from 'fs'
 import { __dirname, config, output } from './webpack.common.js'
+
+const packageJSON = JSON.parse(fs.readFileSync('./package.json'))
 
 export default env => {
   if (env.production) {
@@ -22,6 +25,17 @@ export default env => {
         new webpack.DefinePlugin({
           'process.env.NODE_ENV': JSON.stringify('production'),
         }),
+        new webpack.BannerPlugin(`  Log Right Here, Right Now! HyperLog v${
+          packageJSON.version
+        }
+
+  / a research project /
+
+  by Peiling Jiang at Creativity Lab UCSD (c)
+  https://creativity.ucsd.edu/
+  ${new Date().getFullYear()}
+
+  MIT License`),
       ],
     }
   }
