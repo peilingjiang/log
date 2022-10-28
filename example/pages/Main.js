224473b13fe983db6cdc7876e83797e904999bbf
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2022-10-28 09:28:44 -0700

    revert examples

--- example/pages/Main.js
+++ example/pages/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+import { testMultipleFiles } from './MainSub.js'
 import './synced.js'
 
 export default class Types extends Component {
@@ -7,7 +8,51 @@ export default class Types extends Component {
     super(props)
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    // console.log('Start logging...')
+
+    log(undefined, null)
+
+    log(['a', ['b', ['c', ['d', ['e']]]]])
+
+    const mySet = new Set([1, 2, 3, 4, 5])
+    log(mySet)
+
+    log(
+      1,
+      'log',
+      [1, 'log'],
+      [
+        1,
+        'log',
+        [
+          "My name is James. What's your name? How are you feeling today?",
+          true,
+          234,
+        ],
+      ]
+    )
+
+    testAnotherTopLevelDeclaration()
+
+    testMultipleFiles()
+
+    for (let i = 0; i < 2; i++) {
+      log(this)
+      for (let j = 0; j < 2; j++) {
+        log('nested loop', j)
+      }
+    }
+
+    for (let i = 0; i < 20; i++)
+      log(Math.round(Math.random() * 200))
+        .element(document.getElementById('home-link'))
+        .warn()
+    for (let i = 0; i < 20; i++)
+      log(Math.round(Math.random() * 200))
+        .element(document.getElementById('home-link'))
+        .error()
+  }
 
   render() {
     return (
@@ -43,3 +88,22 @@ export default class Types extends Component {
 // }
 
 // main()
+
+function testAnotherTopLevelDeclaration() {
+  log("I'm from another top level declaration!")
+
+  for (let i = 0; i < 10; i++) {
+    setTimeout(() => {
+      const data = {
+        value: {
+          viewingDistanceCm: 500 * Math.random(),
+          nearPointCm: { x: Math.random(), y: Math.random() },
+          latencyMs: 100,
+        },
+        timestamp: performance.now(),
+        method: 'Random',
+      }
+      log(data).element(document.getElementById('home-link'))
+    }, Math.random() * 1000)
+  }
+}
---
+++ example/pages/MainSub.js
@@ -0,0 +1,7 @@
+export function testMultipleFiles() {
+  log('test multiple files')
+
+  for (let i = 0; i < 10; i++) {
+    log(`${i} test multiple files`)
+  }
+}
